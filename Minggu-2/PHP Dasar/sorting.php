d3e71f88e7e7588c2ee5edb2b27a15a5a1eb7a1e
M. Galih Pratama <mgalih.118140102@student.itera.ac.id>
2020-11-13 11:07:40 +0700

    Mengumpul tugas minggu-2

---
+++ Minggu-2/PHP Dasar/sorting.php
@@ -0,0 +1,10 @@
+<?php
+    $tugas = array("lanirne", "aduh", "qifuat", "toda", "anevi", "samid", "kifuat");
+    sort($tugas);
+    
+    foreach($tugas as $x){
+        echo $x;
+        echo "<br>";
+    }
+    
+?>
