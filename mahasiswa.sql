01be4862d00fe49ca136c58fd7d56aaa6e347951
M. Galih Pratama <mgalih.118140102@student.itera.ac.id>
2020-11-29 12:11:48 +0700

    mengupload eksport database

--- mahasiswa.sql
+++ mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Nov 2020 pada 05.04
+-- Waktu pembuatan: 29 Nov 2020 pada 06.10
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.10
 
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `slug` varchar(128) NOT NULL,
+  `content` text NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `news`
+--
+
+INSERT INTO `news` (`id`, `title`, `slug`, `content`, `user_id`) VALUES
+(6, 'Masak', 'masak', 'telor goyeng', 1),
+(7, 'mandi', 'mandi', 'sambil :v', 1),
+(8, 'tubes banyak anjay', 'tubes-banyak-anjay', 'edan', 1),
+(9, 'meninggal', 'meninggal', 'gara2 tubes banyak', 2),
+(10, 'Mau punya pacar', 'mau-punya-pacar', 'males jomblo terus\r\n', 2),
+(11, 'aku sayang kamu', 'aku-sayang-kamu', 'udah lupain aja gak penting :)', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `user`
 --
 
@@ -32,21 +58,28 @@ CREATE TABLE `user` (
   `username` varchar(10) NOT NULL,
   `password` varchar(50) NOT NULL,
   `fname` varchar(100) NOT NULL,
-  `level` enum('admin','user') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `fname`, `level`) VALUES
+INSERT INTO `user` (`id`, `username`, `password`, `fname`, `role`) VALUES
 (1, 'admin', 'admin', 'Administrator', 'admin'),
-(2, 'user1', 'user1', 'user pertama', 'user'),
-(3, 'user2', 'user2', 'user kedua', 'user');
+(2, 'user1', 'user1', 'User pertama', 'user'),
+(3, 'user2', 'user2', 'User kedua', 'user');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `slug` (`slug`);
 
 --
 -- Indeks untuk tabel `user`
@@ -59,10 +92,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
